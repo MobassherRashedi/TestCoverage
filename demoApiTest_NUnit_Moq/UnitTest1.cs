d220169e79dc6e1d8f9f89a4eda7758a62231d63
MD Mobassher Rashedi <cse.rashedi@gmail.com>
2025-01-22 23:56:29 +0600

    project init

---
+++ demoApiTest_NUnit_Moq/UnitTest1.cs
@@ -0,0 +1,62 @@
+﻿using demoApi.Services;
+
+namespace demoApiTest_NUnit_Moq
+{
+    public class CalculatorServiceTests
+    {
+        private CalculatorService _calculator;
+
+        [SetUp]
+        public void Setup()
+        {
+            _calculator = new CalculatorService();
+        }
+
+        [Test]
+        public void Add_ShouldReturnCorrectSum()
+        {
+            // Arrange 
+            int a = 10;
+            int b = 2;
+
+            // Act
+            var result = _calculator.Add(a, b);
+
+            // Assert
+            Assert.That(12, Is.EqualTo(result));
+
+        }
+
+        [Test]
+        public void Substract_ShouldReturnCorrectResult()
+        {
+            // Arrange 
+            int a = 10;
+            int b = 2;
+
+            // Act
+            var result = _calculator.Substract(a, b);
+
+            // Assert
+            Assert.That(8, Is.EqualTo(result));
+
+
+        }
+
+        [Test]
+        public void Multiplication_ShouldReturnCorrectResult()
+        {
+            // Arrange 
+            int a = 10;
+            int b = 2;
+
+            // Act
+            var result = _calculator.Multiplication(a, b);
+
+            // Assert
+            Assert.That(20, Is.EqualTo(result));
+
+        }
+    }
+}
+
---
+++ src/demoApi/Services/ICalculatorService.cs
@@ -0,0 +1,10 @@
+﻿namespace demoApi.Services
+{
+    public interface ICalculatorService
+    {
+        int Add(int a, int b);
+        int Substract(int a, int b);
+        int Multiplication(int a, int b);
+        int Division(int a, int b);
+    }
+}
